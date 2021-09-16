325ee452c3ad29a568f60f1c1d5057b28984c7be
Matheus Castro <matheus.castro@ilia.digital>
2021-09-16 19:01:58 -0300

    navigation params

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,17 +8,25 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonLayout: Button
+    private lateinit var buttonNavigationParams: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonLayout = findViewById(R.id.buttonLayout)
+        buttonNavigationParams = findViewById(R.id.buttonNavigationParams)
 
         buttonLayout.setOnClickListener {
             val intent = Intent(this, LayoutActivity::class.java)
             startActivity(intent)
         }
+        buttonNavigationParams.setOnClickListener {
+            val intent = Intent(this, NavigationParamsActivity::class.java)
+            intent.putExtra("key", "Mentoria Carrefour")
+            startActivity(intent)
+        }
 
     }
+
 }
---
+++ app/src/main/java/com/example/myapplication/NavigationParamsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class NavigationParamsActivity : AppCompatActivity() {
+    private lateinit var textNavigationParam: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_params)
+
+        textNavigationParam = findViewById(R.id.text_navigation_param)
+
+        val extras = intent.extras
+        if (extras != null) {
+            val value = extras.getString("key")
+            textNavigationParam.text = value
+        }
+    }
+}
