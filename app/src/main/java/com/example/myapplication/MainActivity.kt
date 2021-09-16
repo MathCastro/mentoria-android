c31aa3f4d8d242919cf51c36188548e0fda62d12
Matheus Castro <matheus.castro@ilia.digital>
2021-09-16 17:17:58 -0300

    navigation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonLayout: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonLayout = findViewById(R.id.buttonLayout)
+
+        buttonLayout.setOnClickListener {
+            val intent = Intent(this, LayoutActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
